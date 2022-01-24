0ea2dfb392c8c23f8879373c4ab51bbdff071ed2
Lee Macri <lee.macri@trinityschools.nz>
2022-01-24 13:28:44 +1300

    Update link

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir ~/TKPSource
 cd ~/TKPSource
-wget https://trinityschools-my.sharepoint.com/:u:/g/personal/lee_macri_trinityschools_nz/EVlCcPH_shxAmtBJDLR76_YB-DCijZ4yN-rNAnRY7zJ_PA?e=HWBuS6
+wget -O main.zip https://github.com/rathkealedgt/tkpkasmjava/archive/refs/heads/main.zip
 unzip master.zip
 rm master.zip
 mv TeachingKidsProgramming.Java-master TeachingKidsProgramming.Java
