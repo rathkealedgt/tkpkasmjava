b8cc3010f50814330d92935ad44577b8ee7ffb63
Lee Macri <lee.macri@trinityschools.nz>
2022-01-24 13:35:14 +1300

    Update folder names

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+# This file can be downloaded by students in their Kasm Ubuntu container. The container should already
+# have the eclipse IDE installed. Students may need to be shown how to chmod and execute the script.
 mkdir ~/TKPSource
 cd ~/TKPSource
-wget -O main.zip https://github.com/rathkealedgt/tkpkasmjava/archive/refs/heads/main.zip
+wget -O master.zip https://github.com/rathkealedgt/tkpkasmjava/archive/refs/heads/main.zip
 unzip master.zip
 rm master.zip
-mv TeachingKidsProgramming.Java-master TeachingKidsProgramming.Java
-cd TeachingKidsProgramming.Java
+mv tkpkasmjava-main tkp-kasm-java
+cd tkp-kasm-java
 unzip eclipse_workspace.zip
