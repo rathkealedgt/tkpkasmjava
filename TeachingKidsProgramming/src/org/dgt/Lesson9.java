2f1796bf8adcd841834eafd4381cb6acd63a7aa2
Lee Macri <lem@rathkeale.school.nz>
2022-02-20 19:55:26 +1300

    Update MessageBox.java

--- TeachingKidsProgramming/src/org/dgt/Lesson9.java
+++ TeachingKidsProgramming/src/org/dgt/Lesson9.java
@@ -1,12 +1,18 @@
 package org.dgt;
 
-import org.dgt.lem.MessagegBox;
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
-public class Lesson9
-{
-  public static void main(String[] args)
-  {
-    //String msg = MessageBox.askForTextInput("What?");
-    //MessageBox.showMessage(msg);
+//import org.dgt.lem.Screen;
+
+//import org.dgt.lem.MessagegBox;
+
+public class Lesson9 {
+  public static void main(String[] args) {
+    Tortoise.setPenColor(PenColors.Reds.PaleVioletRed);
+    for (int counter = 0; counter < 20; counter++) {
+      Tortoise.move(150);
+      Tortoise.turn(100);
+    }
   }
 }
