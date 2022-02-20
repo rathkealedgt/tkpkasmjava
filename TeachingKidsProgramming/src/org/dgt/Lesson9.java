bb07e48866d2c3651b53b3593d4ef9b83d663bad
Lee Macri <lem@rathkeale.school.nz>
2022-02-20 20:22:55 +1300

    20/02/22 Update Lesson9

--- TeachingKidsProgramming/src/org/dgt/Lesson9.java
+++ TeachingKidsProgramming/src/org/dgt/Lesson9.java
@@ -9,6 +9,8 @@ import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class Lesson9 {
   public static void main(String[] args) {
+    Tortoise.show();
+    Tortoise.setSpeed(10);
     Tortoise.setPenColor(PenColors.Reds.PaleVioletRed);
     for (int counter = 0; counter < 20; counter++) {
       Tortoise.move(150);
