3cdc1b347f7bf2690cee66387fa5b5480086e6d2
Lee Macri <lem@rathkeale.school.nz>
2022-03-02 09:27:53 +1300

    Update Lesson9.java
    Added new examples.

--- TeachingKidsProgramming/src/org/dgt/Lesson9.java
+++ TeachingKidsProgramming/src/org/dgt/Lesson9.java
@@ -1,20 +1,59 @@
 package org.dgt;
 
-import org.teachingextensions.logo.Tortoise;
-import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import javax.swing.JOptionPane;
 
-//import org.dgt.lem.Screen;
-
-//import org.dgt.lem.MessagegBox;
+import org.dgt.lem.*;
 
 public class Lesson9 {
-  public static void main(String[] args) {
-    Tortoise.show();
-    Tortoise.setSpeed(10);
-    Tortoise.setPenColor(PenColors.Reds.PaleVioletRed);
-    for (int counter = 0; counter < 20; counter++) {
-      Tortoise.move(150);
-      Tortoise.turn(100);
-    }
+  public static void main(String[] args) 
+  {
+    example1();
+    //example2();
+    //example3();
+    //example4();
+    //example5
+    //example6();
   }
+
+  private static void example1()
+  {
+    MessageBox.msgBox("Hello everyone!");
+  }
+
+  private static void example2()
+  {
+    MessageBox.msgBox("Hello Everyone!", "Buttons program");
+  }
+
+  private static void example3()
+  {
+    MessageBox.yesNoBox("Do you like bananas?", "Choose Only 1");
+  }
+
+  private static void example4()
+  {
+    MessageBox.yesNoBox("Do you like bananas?", "Choose Only 1", "TeachingKidsProgramming\\src\\org\\dgt\\img\\bananas.jpg");
+  }
+
+  private static void example5()
+  {
+    int answer = MessageBox.yesNoBox("Do you like bananas?", "Choose Only 1", "TeachingKidsProgramming\\src\\org\\dgt\\img\\bananas.jpg");
+    
+    if(answer == JOptionPane.YES_OPTION)
+      MessageBox.msgBox("Me too!", "Awesome Answer");
+    else
+      MessageBox.msgBox("Don't forget your five a day!", "Poor Answer");
+  }
+
+  public static void example6()
+  {
+    String question = "What is the name of this fruit?";
+    String reply = MessageBox.inputBox(question);
+
+    if(reply.equalsIgnoreCase("BANANA"))
+      MessageBox.msgBox("Correct!");
+    else
+      MessageBox.msgBox("Incorrect");
+  }
+
 }
